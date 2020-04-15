952e0e47e4e46ee011ccfc28714548259261763d
Gary Moore <gary.moore1@ibm.com>
2020-04-15 21:41:08 +0100

    Initial Commit

---
+++ src/test/java/rocks/gazblocks/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.gazblocks.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
